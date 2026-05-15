6fc3375c51aaafdd3ee67ac8be1e9d461dfd4786
aman-ak-r <amanjr042@gmail.com>
2026-05-15 11:20:00 +0530

    feat: implement Prisma 7 WebAssembly PG connection pool adapter

---
+++ backend/src/db/client.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+console.log('🔧 DATABASE_URL:', connectionString);
+
+
+export default prisma;
+export * from '@prisma/client';
