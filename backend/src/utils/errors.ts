317b2be913531d4f1ea59cdb0ec8567dd0d2b689
aman-ak-r <amanjr042@gmail.com>
2026-05-15 13:30:00 +0530

    feat: create base application error hierarchies and application logger

---
+++ backend/src/utils/errors.ts
@@ -0,0 +1,20 @@
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly code: string;
+
+  constructor(message: string, statusCode: number, code: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    Object.setPrototypeOf(this, AppError.prototype);
+  }
+}
+
+export const ErrorCodes = {
+  INVALID_FILE_TYPE: 'INVALID_FILE_TYPE',
+  FILE_TOO_LARGE: 'FILE_TOO_LARGE',
+  UPLOAD_FAILED: 'UPLOAD_FAILED',
+  JOB_NOT_FOUND: 'JOB_NOT_FOUND',
+  INTERNAL_ERROR: 'INTERNAL_ERROR',
+  BAD_REQUEST: 'BAD_REQUEST',
+};
